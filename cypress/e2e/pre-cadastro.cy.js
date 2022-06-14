4f794a3438cc26e6ea864411275c348e42a76a27
Dhionathas Felix <dhionathasqa@gmail.com>
2022-06-14 11:52:42 -0400

    Automatização de novos cenarios

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -6,13 +6,17 @@ describe('Realizar cadastro de login', () => {
   })
 
   it('Deve realizar o cadastro com sucesso', () => {
-    cy.get('#reg_email').type(faker.internet.email())
+    let nomefaker = faker.name.firstName()
+    let sobrenomefaker = faker.name.lastName()
+    let emailfaker = faker.internet.email(nomefaker)
+
+    cy.get('#reg_email').type(emailfaker)
     cy.get('#reg_password').type('Pwd.123')
     cy.get(':nth-child(4) > .button').click()
 
     cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-    cy.get('#account_first_name').type(faker.name.firstName())
-    cy.get('#account_last_name').type(faker.name.lastName())
+    cy.get('#account_first_name').type(nomefaker)
+    cy.get('#account_last_name').type(sobrenomefaker)
     cy.get('.woocommerce-Button').click()
 
     cy.get('.woocommerce-message').should(
