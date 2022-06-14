e95524a27eb43efa3af9cc1ccd9d699c4873017d
Dhionathas Felix <dhionathasqa@gmail.com>
2022-06-13 23:36:01 -0400

    alterações versão do cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,13 @@
-/// <reference types="cypress" />
 
 context('Funcionalidade Login', () => {
-  it('Deve fazer login com sucesso', () => {
+
+  beforeEach(() => {
     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+  });
+  
+  
+
+  it('Deve fazer login com sucesso', () => {
     cy.get('#username').type('aluno_ebac@teste.com')
     cy.get('#password').type('teste@teste.com')
     cy.get('.woocommerce-form > .button').click()
@@ -13,7 +18,6 @@ context('Funcionalidade Login', () => {
   })
 
   it('Deve Exibir mensagem de erro ao inserir senha errada', () => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     cy.get('#username').type('aluno_ebac@teste.com')
     cy.get('#password').type('teste@teste')
     cy.get('.woocommerce-form > .button').click()
@@ -24,7 +28,6 @@ context('Funcionalidade Login', () => {
   })
 
   it('Deve Exibir mensagem de erro ao inserir usuario errado', () => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     cy.get('#username').type('aluno_ebac@.com')
     cy.get('#password').type('teste@teste.com')
     cy.get('.woocommerce-form > .button').click()
